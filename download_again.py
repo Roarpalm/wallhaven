e642284e80a909ee2b7ebb0e16c9fe059c5d5648
Roarpalm <307687673@qq.com>
2020-02-12 14:14:40 +0800

    UpDate

--- download_again.py
+++ download_again.py
@@ -6,7 +6,7 @@ eventlet.monkey_patch()
 
 from bs4 import BeautifulSoup
 from lxml import etree
-import requests, os, time, threading
+import requests, os, time, concurrent.futures
 
 def login():
     global session
@@ -43,10 +43,15 @@ def get_img(url_list):
     global download, session
     for i in url_list:
         with eventlet.Timeout(120,False):
-            download += 1
+
             name = i.split('/')
-            filename = name[-1] + '.jpg'
-            print(f'下载第{download}张图片：{filename}')
+            #判断当前路径是否存在，没有则创建new文件夹
+            b = os.path.abspath('.') + '\\new\\'
+            if not os.path.exists(b):
+                os.makedirs(b)
+            filename = b + name[-1] + '.jpg'
+            print(f'开始下载：{name[-1]}')
+
             img_req = session.get(url = i, headers=headers)
             img_req.encoding = 'utf-8'
             img_html = img_req.content
@@ -56,19 +61,13 @@ def get_img(url_list):
             img_url = img_bf_2.img.get('src')
             response = session.get(img_url, headers=headers)
 
-            b = os.path.abspath('.') + '\\new\\'
-            #判断当前路径是否存在，没有则创建new文件夹
-            if not os.path.exists(b):
-                os.makedirs(b)
-            filename = b + filename
             with open(filename, "wb") as f :
                 f.write(response.content)
 
-            print(f'完成图片：{filename}')
-            success_url.append(i)
-            fail_url = i
-            fail_url_list.remove(fail_url)
-            time.sleep(2)
+            download += 1
+            print(f'第{download}张图片下载完成：{name[-1]}')
+            fail_url_list.remove(i)
+            time.sleep(1)
 
 if __name__ == '__main__':
     # 开始计时
@@ -84,34 +83,22 @@ if __name__ == '__main__':
 
     login()
     print(f'即将下载{len(list_url)}张图片')
-    print(list_url)
 
     fail_url_list = list_url
-    success_url = []
 
     # 将list_url 重写为 包含4个list的list
     n = int(len(list_url) / 4)
-    n2 = n * 2
-    n3 = n * 3
+    n2, n3 = n * 2, n * 3
     new_list = [list_url[0:n], list_url[n:n2], list_url[n2:n3], list_url[n3:len(list_url)]]
 
-    # 开启4个线程
-    th = []
-    th_num = 0
-    for i in new_list:
-        th_num += 1
-        t = threading.Thread(name=f'线程{th_num}', target=get_img, args=(i,))
-        th.append(t)
-    for i in th:
-        i.start()
-    for i in th:
-        i.join()
+    # 线程池开启4个线程
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as e:
+        [e.submit(get_img, i) for i in new_list]
 
-    end_time = time.time()
-    print(f'用时{end_time - start_time}秒')
+    print(f'用时{time.time() - start_time}秒')
 
     if fail_url_list:
         print(f'{len(fail_url_list)}张图片下载失败\n{fail_url_list}')
-    print(f'{len(success_url)}张图片下载成功\n{success_url}')
+    print(f'{len(list_url) - len(fail_url_list)}张图片下载成功')
     
     input('回车以结束程序')
--- wallhaven_setu_machine.py
+++ wallhaven_setu_machine.py
@@ -6,7 +6,7 @@ eventlet.monkey_patch()
 
 from bs4 import BeautifulSoup
 from lxml import etree
-import requests, os, time, threading
+import requests, os, time, concurrent.futures
 
 def login():
     global session
@@ -26,13 +26,13 @@ def login():
 
     data = {
         '_token' : _token,
-        'username': '', # 账号
-        'password': ''  # 密码
+        'username': '',  # 账号
+        'password': ''   # 密码
     }
 
     # 请求登录的url
     login_url = 'https://wallhaven.cc/auth/login'
-  
+ 
     session.post(login_url, headers=headers, data=data)
     print('登录成功')
 
@@ -42,14 +42,14 @@ def get_url():
     '''采集排行榜里的图片链接'''
 
     global list_url, session
-    # 采集的页数，括号内自己改
-    for i in range(1,10):
+    # 采集的页数
+    for i in range(1,6):
         print(f'正在采集第{i}页')
         if i == 1:
-            # 001代表NSFW 1y代表过去一年 toplist
-            url = 'https://wallhaven.cc/search?categories=111&purity=001&topRange=1y&sorting=toplist&order=desc&page'
+            # 001代表NSFW 1y代表过去一年 1M代表过去一月 1w代表过去一周 1d代表过去一天 toplist
+            url = 'https://wallhaven.cc/search?categories=111&purity=001&topRange=6M&sorting=toplist&order=desc&page'
         else:
-            url = f'https://wallhaven.cc/search?categories=111&purity=001&topRange=1y&sorting=toplist&order=desc&page={i}'
+            url = f'https://wallhaven.cc/search?categories=111&purity=001&topRange=6M&sorting=toplist&order=desc&page={i}'
         req = session.get(url=url, headers=headers)
         req.encoding = 'utf-8'
         html1 = req.content
@@ -69,10 +69,15 @@ def get_img(url_list):
     global download, session
     for i in url_list:
         with eventlet.Timeout(120,False):
-            download += 1
+
             name = i.split('/')
-            filename = name[-1] + '.jpg'
-            print(f'{threading.currentThread().getName()}下载第{download}张图片：{filename}')
+            #判断当前路径是否存在，没有则创建new文件夹
+            b = os.path.abspath('.') + '\\new\\'
+            if not os.path.exists(b):
+                os.makedirs(b)
+            filename = b + name[-1] + '.jpg'
+            print(f'开始下载：{name[-1]}')
+
             img_req = session.get(url = i, headers=headers)
             img_req.encoding = 'utf-8'
             img_html = img_req.content
@@ -82,22 +87,14 @@ def get_img(url_list):
             img_url = img_bf_2.img.get('src')
             response = session.get(img_url, headers=headers)
 
-            #判断当前路径是否存在，没有则创建new文件夹
-            b = os.path.abspath('.') + '\\new\\'
-            if not os.path.exists(b):
-                os.makedirs(b)
-            filename = b + filename
             with open(filename, "wb") as f :
                 f.write(response.content)
 
-            print(f'{threading.currentThread().getName()}下载完成：{filename}')
-            success_url.append(i)
-            fail_url = i
-            fail_url_list.remove(fail_url)
-            time.sleep(2)
-
-
-            
+            download += 1
+            print(f'第{download}张图片下载完成：{name[-1]}')
+            fail_url_list.remove(i)
+            time.sleep(1)
+    
 if __name__ == '__main__':
     # 开始计时
     start_time = time.time()
@@ -113,34 +110,22 @@ if __name__ == '__main__':
     login()
     get_url()
     print(f'即将下载{len(list_url)}张图片')
-    print(list_url)
 
     fail_url_list = list_url
-    success_url = []
 
     # 将list_url 重写为 包含4个list的list
     n = int(len(list_url) / 4)
-    n2 = n * 2
-    n3 = n * 3
+    n2, n3 = n * 2, n * 3
     new_list = [list_url[0:n], list_url[n:n2], list_url[n2:n3], list_url[n3:len(list_url)]]
 
-    # 开启4个线程
-    th = []
-    th_num = 0
-    for i in new_list:
-        th_num += 1
-        t = threading.Thread(name=f'线程{th_num}', target=get_img, args=(i,))
-        th.append(t)
-    for i in th:
-        i.start()
-    for i in th:
-        i.join()
+    # 线程池开启4个线程
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as e:
+        [e.submit(get_img, i) for i in new_list]
 
-    end_time = time.time()
-    print(f'用时{end_time - start_time}秒')
+    print(f'用时{time.time() - start_time}秒')
 
     if fail_url_list:
         print(f'{len(fail_url_list)}张图片下载失败\n{fail_url_list}')
-    print(f'{len(success_url)}张图片下载成功\n{success_url}')
+    print(f'{len(list_url) - len(fail_url_list)}张图片下载成功')
     
     input('回车以结束程序')
